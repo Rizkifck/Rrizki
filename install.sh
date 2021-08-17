186a94d0a7fda756ff65fd7c34401ae07edeacf9
Rrizki <88756178+Rizkifck@users.noreply.github.com>
2021-08-17 18:28:05 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+pkg update -y
+pkg upgrade -y
+pkg install wget -y
+pkg install ffmpeg -y
+pkg install wget -y
+pkg install imagemagick -y
+pkg install nodejs -y
+pkg install git -y
+pkg install npm -y
+npm install
+npm audit fix
+
